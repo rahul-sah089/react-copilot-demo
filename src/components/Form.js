095a6d5cd0f04c4792c6ef89bc6b7a7ce11f3657
Karan Thorecha <govind@singledose.com>
2024-03-12 23:56:58 +0530

    Changes to form page

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,14 @@
 // react component for the form to add a new authorization request to the table of requests in the app
 
 import React, { useState } from 'react';
-import { Form, Input, Button, DatePicker, Select } from 'antd';
+import { Form, Input, Button, DatePicker, Select, Row, Col, Radio, Alert } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import { Collapse } from 'react-collapse';
+import Paragraph from 'antd/es/typography/Paragraph';
 
 const FormComponent = ({ handleFormSubmit }) => {
     const [form] = Form.useForm();
-    const [formLayout, setFormLayout] = useState('horizontal');
+    const [formLayout, setFormLayout] = useState('vertical');
     const [formValues, setFormValues] = useState({});
 
     const onFormLayoutChange = ({ layout }) => {
@@ -23,19 +25,69 @@ const FormComponent = ({ handleFormSubmit }) => {
     };
 
     return (
-        <div className='App'>
-            
+        <div className='App btn-secondary'>
+            <Row>
+                <Col span={24}>
+                    <h4>PHP Prior Authorization Form</h4>
             <Form
                 form={form}
                 layout={formLayout}
                 initialValues={{ layout: formLayout }}
+                colon={false}
                 onValuesChange={onFormChange}
                 onFinish={onFormSubmit}
             >
-                <Form.Item label="Auth Request No" name="authRequestNo">
-                    <Input />
+                <Form.Item 
+                label="Authorization Request" 
+                name="authRequestType"
+                rules={[{ required: true, message: 'Please enter an authorization request type' }]}
+                >
+                    <Radio.Group
+                    defaultValue={formValues.authRequestType}
+                    buttonStyle='solid'
+                    onChange={e => setFormValues({ ...formValues, authRequestType: e.target.value })}
+                    >
+                        <Radio.Button value="medical">Medical</Radio.Button>
+                        <Radio.Button value="bh">Behavioral Health</Radio.Button>
+                    </Radio.Group>
                 </Form.Item>
-                <Form.Item label="Status" name="status">
+                <Collapse isOpened={true}>
+                    <Alert message={
+                        <div>
+                            <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed urna eu velit gravida viverra. Fusce et eros odio. Fusce posuere nisi quis diam euismod tempor. Donec lacus dolor, ultrices quis tempus id, cursus id turpis. Praesent quis viverra leo. Nulla pretium nunc quis orci gravida vehicula. Sed faucibus turpis ex, ac semper ex facilisis sed. Duis sed elit at nibh suscipit auctor. Pellentesque mattis enim urna, a varius turpis luctus non.</Paragraph>
+                        </div>
+                    } type="error" />
+                </Collapse>
+                <Form.Item 
+                label="Medical Health Authorization Type" 
+                name="medicalHealthAuthType"
+                rules={[{ required: true, message: 'Please enter an authorization request type' }]}
+                >
+                    <Radio.Group
+                    defaultValue={formValues.authRequestType}
+                    buttonStyle='solid'
+                    onChange={e => setFormValues({ ...formValues, authRequestType: e.target.value })}
+                    >
+                        <Radio.Button value="outpatient">Outpatient</Radio.Button>
+                        <Radio.Button value="dmeopp">DME/OP</Radio.Button>
+                        <Radio.Button value="inpatient-fs">Inpatient Future Stay</Radio.Button>
+                        <Radio.Button value="ip-admitted">Inpatient Admitted</Radio.Button>
+                        <Radio.Button value="obstay">Observation Stay</Radio.Button>
+                        <Radio.Button value="snfltac">SNF/LTAC/Rehab</Radio.Button>
+                    </Radio.Group>
+                </Form.Item>
+                <Collapse isOpened={true}>
+                    <Alert message={
+                        <div>
+                            <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed urna eu velit gravida viverra. Fusce et eros odio. Fusce posuere nisi quis diam euismod tempor. Donec lacus dolor, ultrices quis tempus id, cursus id turpis. Praesent quis viverra leo. Nulla pretium nunc quis orci gravida vehicula. Sed faucibus turpis ex, ac semper ex facilisis sed. Duis sed elit at nibh suscipit auctor. Pellentesque mattis enim urna, a varius turpis luctus non.</Paragraph>
+                        </div>
+                    } type="success" />
+                </Collapse>
+                <Form.Item 
+                label="Status" 
+                name="status"
+                rules={[{ required: true, message: 'Please select a status' }]}
+                >
                     <Select>
                         <Select.Option value="Pending">Pending</Select.Option>
                         <Select.Option value="Approved">Approved</Select.Option>
@@ -63,6 +115,8 @@ const FormComponent = ({ handleFormSubmit }) => {
                     </Button>
                 </Form.Item>
             </Form>
+            </Col>
+            </Row>
         </div>
     );
 }
--- src/components/FormMainStepper.js
+++ src/components/FormMainStepper.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import { Carousel, Radio } from 'antd';
+import { BackTop, Breadcrumb, Button, Carousel, Col, Layout, Radio, Row, Steps } from 'antd';
+import { ControlOutlined, UserOutlined, MedicineBoxOutlined, RightOutlined, TeamOutlined, FileDoneOutlined } from '@ant-design/icons';
 import FormComponent from './Form';
+import { Content } from 'antd/es/layout/layout';
+
+const mainLayout = { sm: { span: 24, offset: 0 }, md: { span: 22, offset: 1 }, xl: { span: 20, offset: 2 } };
 const contentStyle = {
   height: '100vh',
   color: '#000',
@@ -12,29 +16,72 @@ const contentStyle = {
 };
 
 const FormMainStepper = ({ handleFormSubmit }) => {
-    const [dotPosition, setDotPosition] = useState('top');
+  const [current, setCurrent] = useState(0);
+  const onStepsChange = current => {
+    console.log('onChange:', current);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  }
+  const next = () => {
+    setCurrent(current + 1);
+  }
 
-    return (<>
-        <Header />
-        <>
-      <Carousel dotPosition={dotPosition}>
-        <div>
-          <h3 style={contentStyle}>
-            <FormComponent handleFormSubmit={handleFormSubmit} />
-          </h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-          <h3 style={contentStyle}>4</h3>
-        </div>
-      </Carousel>
-    </>
-    </>)
+  return (<>
+    <Layout>
+      <BackTop visibilityHeight={100} />
+      <Header />
+      <Content style={{ padding: '0 10%' }}>
+        <Row justify="center">
+          <Col {...mainLayout}>
+            <Row justify="center">
+              <Col span={24}>
+                <Breadcrumb>
+                  <Breadcrumb.Item>
+                    <Button type="link" href="/">Home</Button>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                    <Button type="link" href="/">PHP Prior Authorization Submission</Button>
+                  </Breadcrumb.Item>
+                </Breadcrumb>
+              </Col>
+              <Col span={24}>
+            <Steps
+              current={current}
+              onChange={onStepsChange}
+            >
+              <Steps.Step title="Step 1" description="Authorization type" icon={<ControlOutlined />} />
+              <Steps.Step title="Step 2" description="Patient Details" icon={<UserOutlined />} />
+              <Steps.Step title="Step 3" description="Request Details" icon={<MedicineBoxOutlined />} />
+              <Steps.Step title="Step 4" description="Providers" icon={<TeamOutlined />} />
+              <Steps.Step title="Step 5" description="Confirm Request" icon={<FileDoneOutlined />} />
+            </Steps>
+            <Carousel >
+              <div className='steps-content'>
+                <h3 style={contentStyle}>
+                  <FormComponent handleFormSubmit={handleFormSubmit} />
+                </h3>
+              </div>
+              <div className='steps-content'>
+                <h3 style={contentStyle}>2</h3>
+              </div>
+              <div className='steps-content'>
+                <h3 style={contentStyle}>3</h3>
+              </div>
+              <div className='steps-content'>
+                <h3 style={contentStyle}>4</h3>
+              </div>
+            </Carousel>
+            {/* <Button type="primary" onClick={prev}>Previous</Button>
+            <Button type="primary" onClick={next}>Next</Button> */}
+            <Button type="primary" size='large' htmlType='submit' icon={<RightOutlined />} onClick={next}>Submit</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  </>)
 };
 
 export default FormMainStepper;
